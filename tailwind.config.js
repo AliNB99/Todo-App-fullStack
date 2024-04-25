b0dea4f0d2875b28f9b0e45cdcf8ac93d25efaa7
ali nadimi <alinb99.dev@gmail.com>
2024-04-26 00:56:05 +0330

    start project and set config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        normal: "0px 0px 10px 1px #e2e2e2",
+      },
+    },
+  },
+  plugins: [],
+};
